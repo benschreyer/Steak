4c43505edb73d1e460a444b49ffe8e57ef874f6d
bensch <crafte23@gmail.com>
2021-03-30 20:21:21 -0400

    fix tooltips

--- index.js
+++ index.js
@@ -125,7 +125,9 @@ function retreiveContract() {
     contract = new web3.eth.Contract(abi, $("#newInfo").val().trim());
     console.log(contract);
     document.getElementById("addressLabel").innerHTML = "Contract Address: " + '<a target="_blank" href = "https://kovan.etherscan.io/address/' + $("#newInfo").val() + '">' + $("#newInfo").val().trim() + "</a>" + '<a data-container="body" data-toggle="tooltip" data-placement="top" title="Copy Shareable Steak View Link" style = "float:right;verical-algin:center"  onclick ="shareableLink()"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16"><path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/><path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/></svg></a>';
-
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      });
     contract.methods.dataScientistModelName().call().then(function(info) {
         
 
