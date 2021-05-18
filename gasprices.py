25ba71466e86cb889bd0a1156d60388bd45f7285
smapapurf <crafte23@gmail.com>
2021-05-18 17:33:06 -0400

    Update gasprices.py

--- gasprices.py
+++ gasprices.py
@@ -5,11 +5,11 @@ seconds_per_day = 86400
 gwei = 1000000000
 prices = []
 time_estimates = []
-for i in range(1,101):
+for i in range(70,120):
     print(i)
     prices.append(gwei * i)
-    time_estimates.append(float(requests.get("https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=VRY48ZXPBVJZDAW276IB937P338GD4IQXS").json()["result"])/seconds_per_day)
+    time_estimates.append(float(requests.get("https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice="+ str(gwei * i) + "&apikey=VRY48ZXPBVJZDAW276IB937P338GD4IQXS").json()["result"])/seconds_per_day)
     if(i % 4 == 0):time.sleep(1)
 plt.plot(prices,time_estimates)
 plt.show()
-print(requests.get("https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=VRY48ZXPBVJZDAW276IB937P338GD4IQXS").json())
+print(requests.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=VRY48ZXPBVJZDAW276IB937P338GD4IQXS").json())
