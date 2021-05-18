7aa3a1a489c31e78d3a88a305cea02f7bc25277a
smapapurf <crafte23@gmail.com>
2021-05-17 20:13:40 -0400

    email subscription modal and js
    subscribe to get emails if a contract fails (no submissions, incorrect submission, no stake)

--- ServerPyCode/check_cont_fail.py
+++ ServerPyCode/check_cont_fail.py
@@ -7,7 +7,7 @@ def check_line(lin):
     saved_user_string = lin.split(",")
     if(len(saved_user_string) == 5):
         #print(requests.get("https://api-tournament.numer.ai/graphql?query={rounds{number}}").json())
-        roundNum = requests.get("https://api-tournament.numer.ai/graphql?query={rounds{number}}").json()["data"]["rounds"][0]["number"] - 1
+        roundNum = requests.get("https://api-tournament.numer.ai/graphql?query={rounds{number}}").json()["data"]["rounds"][0]["number"]
         controlBuyer = None
         try:
             controlBuyer = requests.get("https://api-tournament.numer.ai/?query={v2UserProfile(username:\""+ saved_user_string[1] +"\"){control}}").json()["data"]["v2UserProfile"]["control"]
@@ -30,6 +30,6 @@ def check_line(lin):
             sellerCorr = requests.get("https://api-tournament.numer.ai/graphql?query={roundSubmissionPerformance(roundNumber:"+str(roundNum)+",username:\""+saved_user_string[0]+"\"){roundDailyPerformances{correlation}}}").json()["data"]["roundSubmissionPerformance"]["roundDailyPerformances"][-1]["correlation"]
             buyerCorr = requests.get("https://api-tournament.numer.ai/graphql?query={roundSubmissionPerformance(roundNumber:"+str(roundNum)+",username:\""+saved_user_string[1]+"\"){roundDailyPerformances{correlation}}}").json()["data"]["roundSubmissionPerformance"]["roundDailyPerformances"][-1]["correlation"]
             if(buyerCorr != sellerCorr):
-                print("CORRELATIONS DONT MATCH CONTRACT NOT HONORED")
+                print("CORRELATIONS DONT MATCH CONTRACT NOT HONORED",type(buyerCorr))
     #print(roundNum,controlBuyer,controlSeller,sellerStake,sellerCorr,buyerCorr)
 check_line("bensch_c,bensch_a,1.31232,9932.213,testmay2@gmail.com")
