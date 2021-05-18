f3e12a73d27ab55f90934eb4bd2be8e2c789be12
smapapurf <crafte23@gmail.com>
2021-05-17 20:29:43 -0400

    check email list and send warnings script

--- ServerPyCode/check_cont_fail.py
+++ ServerPyCode/check_cont_fail.py
@@ -1,6 +1,9 @@
 import requests
 #"data.rounds.0.number"
-
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import time
 
 
 def check_line(lin):
@@ -25,11 +28,53 @@ def check_line(lin):
             pass
 
         if(controlBuyer is None or controlSeller is None or float(sellerStake) < float(saved_user_string[2])):
-            print("CONTRACT WAS NOT HONORED, MODELS LATE/UNSUBMITTED OR SELLER DID NOT STAKE AS MUCH AS THEY PROMISED")
+            return "It appears the models " + saved_user_string[0] + " and " + saved_user_string[1] + " were not submitted on time or at all, or the seller did not stake as much as they promised to. You should confirm this on numer.ai and request a contract closure if this was unintended."
         else:
             sellerCorr = requests.get("https://api-tournament.numer.ai/graphql?query={roundSubmissionPerformance(roundNumber:"+str(roundNum)+",username:\""+saved_user_string[0]+"\"){roundDailyPerformances{correlation}}}").json()["data"]["roundSubmissionPerformance"]["roundDailyPerformances"][-1]["correlation"]
             buyerCorr = requests.get("https://api-tournament.numer.ai/graphql?query={roundSubmissionPerformance(roundNumber:"+str(roundNum)+",username:\""+saved_user_string[1]+"\"){roundDailyPerformances{correlation}}}").json()["data"]["roundSubmissionPerformance"]["roundDailyPerformances"][-1]["correlation"]
             if(buyerCorr != sellerCorr):
-                print("CORRELATIONS DONT MATCH CONTRACT NOT HONORED",type(buyerCorr))
+                return "It appears the correlations of models " + saved_user_string[0] + " and " + saved_user_string[1] + " do not match. You should confirm this on numer.ai and request a contract closure if this was unintended."
+    return ""
+
+def send_warning(ema,msg):
+        #The mail addresses and password
+        
+        mail_content = msg
+        sender_address = "bogachup1234@gmail.com"
+        sender_pass = 'xxx'
+        receiver_address = ema
+        #Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = sender_address
+        message['To'] = receiver_address
+        message['Subject'] = 'Steak Contract Failure Warning.\n'    #The subject line
+        #The body and the attachments for the mail
+        message.attach(MIMEText(mail_content, 'plain'))
+        #Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+        session.starttls() #enable security
+        session.login(sender_address, sender_pass) #login with mail_id and password
+        text = message.as_string()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
+        print('Mail Sent')
+
+
+
     #print(roundNum,controlBuyer,controlSeller,sellerStake,sellerCorr,buyerCorr)
-check_line("bensch_c,bensch_a,1.31232,9932.213,testmay2@gmail.com")
+#check_line("bensch_c,bensch_a,1.31232,9932.213,testmay2@gmail.com")
+
+text_file = open("emails.txt", "r")
+ 
+#read whole file to a string
+emails = text_file.read()
+ 
+#close file
+text_file.close()
+
+for lne in emails.split("\n"):
+    check = check_line(lne)
+    if(len(check) > 0):
+        send_warning(lne.split(",")[-1],check)
+ 
+
