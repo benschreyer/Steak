8871f91cfcfb006bb6ba755eb828b227ecf4d89e
bensch <crafte23@gmail.com>
2021-05-23 21:01:43 -0400

    Update SteakQuarterly.sol

--- SteakQuarterly/SteakQuarterly.sol
+++ SteakQuarterly/SteakQuarterly.sol
@@ -6,6 +6,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice (including the next paragraph) shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+//5/23/2021 DEBUG TIMING CONDITIONS PRESENT NOT FOR REAL USE 
 
 //Last updated 4/1/2021
 //bensch
@@ -326,8 +327,8 @@ contract SteakQuarterly is ChainlinkClient
         
         require(msg.sender == owner, "Only the owner can trigger a payment claim.");
         require(locked, "Cannot claim an unlocked contract.");
-
-        require(((tempStamp - startTimestamp) / 604800) > 1, "Cannot claim contract before 12 weeks have elapsed.");
+        //DEBUG VERSION SET TO 11 when LAUNCHING
+        require(((tempStamp - startTimestamp) / 604800) > 0, "Cannot claim contract before 12 weeks have elapsed.");
 
         
         emit Claimed();
